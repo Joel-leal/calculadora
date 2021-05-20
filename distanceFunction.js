91943a465c00183a0ac8b1d9b7ee0483ad6d5cf7
Joel-leal <joel.lasi@hotmail.com>
2021-05-20 14:15:24 -0300

    configurando código da distanceFunction.js

--- distanceFunction.js
+++ distanceFunction.js
@@ -1,3 +1,22 @@
+const listaMedidas = [
+  { text: "km", value: 0.1 },
+  { text: "hm", value: 0.01 },
+  { text: "dam", value: 0.001 },
+  { text: "m", value: 0.0001 },
+  { text: "dm", value: 0.00001 },
+  { text: "cm", value: 0.000001 },
+  { text: "mm", value: 0.0000001 },
+];
+const selectElements = Array.from(
+  document.getElementsByClassName("form-select")
+); //Array.from pega os elementos como de uma lista
+listaMedidas.map((item) => {
+  console.log(item);
+  // selectElements.innerHTML += `<option value="1">${item}</option>`;
+  selectElements.map((element) => {
+    element.innerHTML += `<option value="${item.value}">${item.text}</option>`;
+  });
+});
 function handleChange() {
   let outputElement = document.getElementById("output");
   let inputValue = new Decimal(
